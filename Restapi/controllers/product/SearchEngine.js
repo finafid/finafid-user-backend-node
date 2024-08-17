36e686afbc6af35dff40b0fd4fbd87e9be5a9150
Deep <deep123aeccse@gmail.com>
2024-08-17 19:18:58 +0530

    Search type

--- Restapi/controllers/product/SearchEngine.js
+++ Restapi/controllers/product/SearchEngine.js
@@ -86,12 +86,9 @@ const Variant = require("../../models/product/Varient");
 const searchEngineLink=process.env.searchEngineLink
 const getSearchDataFirst = async (req, res) => {
   try {
-    const response = await axios.post(
-      "https://finafid-search-engine-b5dc68ef0eb1.herokuapp.com/search/",
-      {
-        query: req.query.query,
-      }
-    );
+    const response = await axios.post("http://laptop-uptfb6dh:8000/search/", {
+      query: req.query.query,
+    });
     let stringList=[]
    
     if (response.data.message){
@@ -127,6 +124,7 @@ const getSearchDataSecond = async (req, res) => {
     );
 
     let variantList = [];
+    console.log(response.data.results);
     for (let element of response.data.results) {
       const productDetails = await productSc
         .findOne({
