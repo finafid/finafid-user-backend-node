a4ce46a9887027d2548252023357462fb3459857
Deep <deep123aeccse@gmail.com>
2024-08-22 18:38:48 +0530

    Payu solve issue

--- Restapi/controllers/Payment/PayuPaymentController.js
+++ Restapi/controllers/Payment/PayuPaymentController.js
@@ -3,14 +3,16 @@ const PAYU_MERCHANT_SALT = process.env.PAYU_MERCHANT_SALT;
 const PAYU_BASE_URL = process.env.PAYU_BASE_URL;
 const User = require("../../models/auth/userSchema");
 const crypto = require("crypto");
+const Order=require("../../models/Order/orderSc")
 const paymentDetail = async (req, res) => {
   try {
-    console.log(req.body)
-    console.log(PAYU_MERCHANT_KEY);
-    console.log(PAYU_MERCHANT_SALT);
     const { amount, orderId } = req.body;
+    const orderDetails = await Order.findById(orderId);
     const userDetails = await User.findById(req.user._id);
-    const txnid = "" + orderId;
+    // const txnid = "" + orderDetails.transactionId;
+    // console.log(orderDetails.transactionId);
+     const txnid = "" + orderId;
+    console.log(txnid);
     const hashString = `${PAYU_MERCHANT_KEY}|${txnid}|${amount}|Order|${userDetails.fullName}|${userDetails.email}|||||||||||${PAYU_MERCHANT_SALT}`;
     const hash = crypto.createHash("sha512").update(hashString).digest("hex");
     console.log("Generated Hash: ", hash);
@@ -22,8 +24,8 @@ const paymentDetail = async (req, res) => {
       firstname: userDetails.fullName,
       email: userDetails.email,
       phone: userDetails.phone.toString(),
-      surl: "https://finafid.com/account/orders",
-      furl: "https://finafid.com/account/orders",
+      surl: "http://localhost:3000/api/v1/paymentResponse",
+      furl: "http://localhost:3000/api/v1/paymentResponse",
       hash: hash,
       service_provider: "payu_paisa",
     };
@@ -37,15 +39,59 @@ const paymentDetail = async (req, res) => {
 };
 
 const paymentResponse = async (req, res) => {
-  const hashString = `${PAYU_MERCHANT_SALT}|${req.body.paymentData.status}|||||||||||${req.body.paymentData.email}|${req.body.paymentData.firstname}|${req.body.paymentData.productinfo}|${req.body.paymentData.amount}|${req.body.paymentData.txnid}|${PAYU_MERCHANT_KEY}`;
-  const hash = crypto.createHash("sha512").update(hashString).digest("hex");
-  console.log({ hash: hash });
-  if (hash === req.body.hash) {
-    res.send("Payment successful");
-  } else {
-    res.send("Payment verification failed");
+  try {
+    console.log("kedkjmfkem")
+    const { txnid, status, amount, email, firstname, productinfo, hash } =
+      req.body.paymentData;
+    consol.log(req.body)
+    if (
+      !txnid ||
+      !status ||
+      !amount ||
+      !email ||
+      !firstname ||
+      !productinfo ||
+      !hash
+    ) {
+      return res.status(400).send("Invalid payment data");
+    }
+
+    const hashString = `${PAYU_MERCHANT_SALT}|${status}|||||||||||${email}|${firstname}|${productinfo}|${amount}|${txnid}|${PAYU_MERCHANT_KEY}`;
+    const generatedHash = crypto
+      .createHash("sha512")
+      .update(hashString)
+      .digest("hex");
+
+    console.log({ generatedHash, receivedHash: hash });
+
+    if (generatedHash === hash) {
+      if (status === "success") {
+        const updatedOrder = await Order.findOneAndUpdate(
+          { _id: txnid },
+          { payment_complete: true, status: "Confirmed" },
+          { new: true }
+        );
+
+        res.render("paymentSuccess", { order: updatedOrder });
+      } else {
+        const updatedOrder = await Order.findOneAndUpdate(
+          { _id: txnid },
+          { status: "Failed" },
+          { new: true }
+        );
+
+        res.render("paymentFailure", { order: updatedOrder });
+      }
+    } else {
+      res.status(400).send("Payment verification failed");
+    }
+  } catch (error) {
+    console.error("Error processing payment response:", error);
+    res.status(500).send("Internal Server Error");
   }
 };
+
+
 module.exports = {
   paymentResponse,
   paymentDetail,
