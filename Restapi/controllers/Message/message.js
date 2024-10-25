f7fa0215ee2f52858cb948291a23022a93921629
Deep Chakraborty <deep123aeccse@gmail.com>
2024-10-25 17:12:10 +0530

    sub categoryFixed

--- Restapi/controllers/Message/message.js
+++ Restapi/controllers/Message/message.js
@@ -4,6 +4,7 @@ const { sendSMS } = require("../../middlewares/message");
     const templateId = "1007328752894505628";
     const message ="Your FINAFID Order has been Delivered. Enjoy your products. Thank you for shopping with FINAFID";
     const responseDetails = await sendSMS(message, req.body.phoneNumber, templateId);
+    console.log(responseDetails.data)
     return res.status(200).json({message:"Send successfully"})
     }catch(error){
         res.status(500).json({
