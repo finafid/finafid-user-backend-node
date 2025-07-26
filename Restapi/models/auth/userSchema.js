3a52c12bc0e6f312686ca87b069ba4a4242b604f
Mahesh <deb@ordering.agency>
2025-07-26 13:37:21 +0530

    sfsd

--- Restapi/models/auth/userSchema.js
+++ Restapi/models/auth/userSchema.js
@@ -69,5 +69,5 @@ const UserSchema = new Schema(
   },
   { timestamps: true }
 );
-const userModel = mongoose.model("User", UserSchema);
+const userModel = mongoose.model("user", UserSchema);
 module.exports = userModel;
