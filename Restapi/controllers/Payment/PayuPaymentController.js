8bf2242396e776e61eeec311083676a47acabddd
Mahesh <deb@ordering.agency>
2025-08-02 23:00:25 +0530

    UPDATE PAYMENT

--- Restapi/controllers/Payment/PayuPaymentController.js
+++ Restapi/controllers/Payment/PayuPaymentController.js
@@ -18,7 +18,7 @@ const Wallet = require("../../models/Wallet/wallet");
 const walletTransaction = require("../../models/Wallet/WalletTransaction");
 
 const { getSocketInstance } = require("../order/socket");
-const { updateStatusv2, updateNewStatusv2 } = require("../order/orderControllerv2");
+const { updateNewStatusv2 } = require("../order/orderControllerv2");
 // Payment Request (Order or Wallet)
 const paymentDetail = async (req, res) => {
   try {
@@ -92,7 +92,7 @@ const newpaymentDetail = async (req, res) => {
       productinfo = "Order Payment";
     }
     console.log("Payment Mode:", orderId);
-    const hashString = `${PAYU_MERCHANT_KEY}|${txnid}|${amount}|${productinfo}|${userDetails.fullName}|${userDetails.email}|||||||||||${PAYU_MERCHANT_SALT}`;
+    const hashString = `${PAYU_MERCHANT_SALT}|${status}|||||||||||${email}|${firstname}|${productinfo}|${amount}|${txnid}|${PAYU_MERCHANT_KEY}`;
     const hash = crypto.createHash("sha512").update(hashString).digest("hex");
 
     const paymentData = {
